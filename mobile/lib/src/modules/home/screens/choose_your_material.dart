22f537b39e80c1e1626d2a9d05bf7bd1d4bc48fb
dungngminh <ngminhdung1311@gmail.com>
2022-03-12 00:31:34 +0700

    feat(SC-78): base state managament and di

--- mobile/lib/src/modules/home/screens/choose_your_material.dart
+++ mobile/lib/src/modules/home/screens/choose_your_material.dart
@@ -1,16 +1,13 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:mobile/src/core/config/router.dart';
 import 'package:mobile/src/core/theme/custom_text_theme.dart';
 import 'package:mobile/src/core/theme/palette.dart';
-import 'package:mobile/src/data/repositories/ingredient_repository.dart';
 import 'package:mobile/src/modules/home/screens/test_screen.dart';
 import 'package:mobile/src/modules/home/widgets/material_card.dart';
 import 'package:mobile/src/widgets/custom_back_button.dart';
 import 'package:mobile/src/widgets/no_show_limit_scroll.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'package:provider/provider.dart';
 
 class ChooseYourMaterial extends StatefulWidget {
   const ChooseYourMaterial({Key? key}) : super(key: key);
