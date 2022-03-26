e74b0b09ac5f6fc11c9c1fb6b1b24a18f346f9c4
Nguyen Minh Dung <63831488+dungngminh@users.noreply.github.com>
2022-03-26 15:47:44 +0700

    fix(SC-106): fix bugggggggg

--- mobile/lib/src/data/repositories/ingredient_repository.dart
+++ mobile/lib/src/data/repositories/ingredient_repository.dart
@@ -16,25 +16,38 @@ class IngredientRepository {
           .getMethod("/ingredients?page=$page&size=12&sort=category_id");
       var converted = jsonDecode(dataRaw);
       Iterable data = converted["content"];
-      log(data.toString());
       return data.map((value) => Ingredient.fromJson(value)).toList();
     } catch (e) {
       log("gete List Ingredients fail ${e.toString()}");
       throw Exception("get List Ingredients fail");
     }
   }
-  
-  Future<List<Ingredient>> searchIngredients(String value) async{
+
+  Future<List<Ingredient>> searchIngredients(String value) async {
     try {
-      final dataRaw = await baseApi
-          .getMethod("/ingredients/?search=$value");
+      final dataRaw = await baseApi.getMethod("/ingredients/?search=$value");
       var converted = jsonDecode(dataRaw);
       Iterable data = converted["content"];
-      log(data.toString());
       return data.map((value) => Ingredient.fromJson(value)).toList();
     } catch (e) {
-      log("gete List Ingredients fail ${e.toString()}");
-      throw Exception("get List Ingredients fail");
+      log("Search List Ingredients fail ${e.toString()}");
+      throw Exception("Search Ingredients $value fail");
     }
   }
- }
+
+  Future<List<Ingredient>> getListIngredientByCategory(int categoryId) async {
+    try {
+      final dataRaw = await baseApi.postMethod(
+        "/ingredients/categories",
+        body: <String, List<int>>{
+          "ids": <int>[categoryId]
+        },
+      );
+      Iterable data = jsonDecode(dataRaw);
+      return data.map((value) => Ingredient.fromJson(value)).toList();
+    } catch (e) {
+      log("post List Ingredients by category fail ${e.toString()}");
+      throw Exception("post List Ingredients by category fail $categoryId");
+    }
+  }
+}
